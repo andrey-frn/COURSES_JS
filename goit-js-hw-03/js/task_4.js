c609ad194641aaabd26ac0975ff8c82af41c50a3
Andrey Borodenkov <borodenkov.andrey@gmail.com>
2020-07-30 13:24:12 +0300

    hw_3

---
+++ goit-js-hw-03/js/task_4.js
@@ -0,0 +1,28 @@
+const countTotalSalary = function(employees) {
+  let totalValue = 0;
+for (const key in employees) {
+  totalValue += employees[key];
+}
+return totalValue;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
